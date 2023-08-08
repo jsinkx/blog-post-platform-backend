fe333d8b7e77039bccdd56b95b7bb1c4c34cd9b5
Artemiy <andrikv@gmail.com>
2023-08-08 21:14:56 +0300

    Added last name in auth-register-validations.ts

--- src/validations/auth/auth-register-validations.ts
+++ src/validations/auth/auth-register-validations.ts
@@ -8,11 +8,12 @@ const authRegisterValidation = [
 			max: 25,
 		}),
 	body('firstName', 'Please provide your first name').isString(),
+	body('lastName', 'Please provide your last name').optional().isString(),
+	body('patronymic', 'Please provide valid patronymic').optional().isString(),
 	body('email', 'Please provide your email').isEmail(),
 	body('password', 'Please provide password with length greater than 5 symbols')
 		.isString()
 		.isLength({ min: 5 }),
-	body('patronymic', 'Please provide valid patronymic').optional().isString(),
 ]
 
 export default authRegisterValidation
